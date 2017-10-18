d5ead959ce4081e2dc336481368c42fd7b132a57
Jim Haff <Jimmyhaff3@gmail.com>
2017-10-18 10:53:28 -0500

    create index and show page for articles controller

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/authors')
 
 router.get('/', (req, res) => {
-  res.render('index', {})
+  Author.find((err, authors) => {
+      res.render('authors/index', {authors: authors})
+  })
+
 })
+
+router.post('/', (req, res) => {
+  Author.create(req.body, (err, author) => {
+    res.redirect('/authors')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('authors/new', {})
+})
+
+
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const methodOverride = require('method-override');
 require('./db/db')
 
 const authorController = require('./controllers/authors');
+const homeController  = require('./controllers/home')
+const articlesController = require('./controllers/articles');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -17,7 +19,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static('public'))
 
+app.use('/articles', articlesController);
 app.use('/authors', authorController);
+app.use('/', homeController);
+
+
 
 app.listen(3000, () => {
   console.log('server is listening at port 3000')
